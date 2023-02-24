6366dde79532310b052f2b7e29e403b4ed81ebcd
shahidullah piaro <shahidullah.piyaro@gmail.com>
2023-02-24 16:56:16 +0600

    heap sort

--- Module32/heapSort.cpp
+++ Module32/heapSort.cpp
@@ -1,7 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class MaxHeap{
+class MaxHeap
+{
 public:
     vector<int>nodes;
 
@@ -15,7 +16,7 @@ public:
     {
         while(idx > 0 && nodes[idx] > nodes[(idx-1)/2])
         {
-            swap(nodes[idx] , nodes[(idx-1)/2]);
+            swap(nodes[idx], nodes[(idx-1)/2]);
             idx = (idx-1)/2;
         }
     }
@@ -29,7 +30,7 @@ public:
     //O(n)
     void PrintHeap()
     {
-        for(int i=0;i<nodes.size();i++)
+        for(int i=0; i<nodes.size(); i++)
         {
             cout<<nodes[i]<<" ";
         }
@@ -54,7 +55,7 @@ public:
             }
             if(largest==idx)
                 break;
-            swap(nodes[idx] , nodes[largest]);
+            swap(nodes[idx], nodes[largest]);
             idx = largest;
         }
 
@@ -65,7 +66,7 @@ public:
     {
         if(idx >= nodes.size())
             return;
-        swap(nodes[idx] , nodes[nodes.size()-1]);
+        swap(nodes[idx], nodes[nodes.size()-1]);
         nodes.pop_back();
         down_heapify(idx);
     }
@@ -93,22 +94,46 @@ public:
         Delete(0);
         return ret;
     }
+
+    //O(n)
+    void build_from_array(vector<int>&a)
+    {
+        nodes = a;
+        int n = nodes.size();
+        int last_non_leaf = n/2 - 1;
+        for(int i=last_non_leaf; i>=0; i--)
+        {
+            down_heapify(i);
+        }
+    }
 };
 
+//O(nlogn)
+vector<int> heap_sort(vector<int>a)
+{
+    MaxHeap h;
+    h.build_from_array(a);
+    vector<int>ans;
+    for(int i=0; i<a.size(); i++)
+    {
+        ans.push_back(h.ExtractMax());
+    }
+    reverse(ans.begin(),ans.end());
+    return ans;
+}
 int main()
 {
-    MaxHeap heap;
-    heap.insert(4);
-    heap.insert(7);
-    heap.insert(9);
-    heap.insert(1);
-    heap.insert(10);
-    heap.insert(20);
-    heap.insert(30);
-    cout<<"Max element = "<<heap.getMax()<<"\n";
-//    heap.PrintHeap();
+//    MaxHeap h;
+//    vector<int>a = {1,2,3,4,10,9,8,7};
+//    h.build_from_array(a);
+//    h.PrintHeap();
 
-    cout<<"Max element = "<<heap.ExtractMax()<<"\n";
-    cout<<"Max element = "<<heap.ExtractMax()<<"\n";
+    vector<int>a = {1,2,3,4,10,9,5,7};
+    vector<int>sorted_a = heap_sort(a);
+    for(int i=0; i<sorted_a.size(); i++)
+    {
+        cout<<sorted_a[i]<<" ";
+    }
+    cout<<"\n";
     return 0;
 }
